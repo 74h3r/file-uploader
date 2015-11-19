1e28d166230785eae6467862fa8258989e5582ce
taher8828 <taher8828@gmail.com>
2015-11-19 08:53:37 -0800

    First source code version.

--- README.md
+++
@@ -1,6 +0,0 @@
-# file-uploader
-File upload and management portal.
-
-# Notes
-This is my first progect to learn software developing.
-
---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>MT Simple IT</title>
+        <meta name="description" content="This is M&T Website">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <nav class="navbar navbar-default container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">MT-Simple-IT</a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#login-col" data-toggle="collapse"><span class="glyphicon glyphicon-log-in"></span> Log In</a></li>
+            </ul>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <div id="login-col" class="collapse">
+                    <form class="form-horizontal" method="post" role="form">
+                        <div class="form-group">
+                            <label class="control-label col-sm-offset-4 col-sm-4" for="uname">User Name:</label>
+                            <div class="col-sm-4">
+                                <input type="text" class="form-control" autofocus="true" required id="uname" name="uname" placeholder="Enter User Name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-sm-offset-4 col-sm-4" for="pwd">Password:</label>
+                            <div class="col-sm-4">
+                                <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Enter Password">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-8 col-sm-4">
+                                <button type="submit" class="btn btn-default">Log In</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="jumbotron">
+                <h1>Futrue is comming ...</h1>
+                <p class="text-right">الو اول ملوش اخر</p>
+            </div>
+        <hr>
+        <footer>
+            <p>&copy; Company 2015</p>
+        </footer>
+        </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+            <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/main.js"></script>
+    </body>
+</html>
